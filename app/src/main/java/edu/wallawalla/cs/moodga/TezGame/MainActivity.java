c172e756aaacd8ca12f958c5b71b0e4d80f0b041
Garrett Moody <extrillieos@gmail.com>
2021-04-12 16:00:30 -0700

    finished lab

--- app/src/main/java/edu/wallawalla/cs/moodga/TezGame/MainActivity.java
+++ app/src/main/java/edu/wallawalla/cs/moodga/TezGame/MainActivity.java
@@ -2,13 +2,22 @@ package edu.wallawalla.cs.moodga.TezGame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(v -> Toast.makeText(MainActivity.this,
+                "You have started!", Toast.LENGTH_SHORT).show());
+
     }
 }

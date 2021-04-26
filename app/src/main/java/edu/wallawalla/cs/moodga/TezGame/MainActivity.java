24f693a5d4daa138e5f3de867ed845c48ab03c75
garrettkmoody <extrillieos@gmail.com>
2021-04-26 14:53:56 -0800

    menu complete

--- app/src/main/java/edu/wallawalla/cs/moodga/TezGame/MainActivity.java
+++ app/src/main/java/edu/wallawalla/cs/moodga/TezGame/MainActivity.java
@@ -1,15 +1,20 @@
 package edu.wallawalla.cs.moodga.TezGame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,12 +22,14 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_GAME_RESULT = 0;
+    private ImageView ghostIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button startButton = findViewById(R.id.startButton);
+        ghostIV = findViewById(R.id.ghostPic);
         EditText enternameET = findViewById(R.id.etUsername);
         TextView ghostnameTV = findViewById(R.id.ghostNameTV);
         startButton.setOnClickListener(v -> {
@@ -48,6 +55,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        // Determine which menu option was chosen
+        if (item.getItemId() == R.id.action_one) {
+            ghostIV.setColorFilter(Color.argb(100, 255, 255, 255));
+            return true;
+        }
+        else if (item.getItemId() == R.id.action_two) {
+            ghostIV.setColorFilter(Color.argb(100, 255, 0, 0));
+            return true;
+        }
+        else if (item.getItemId() == R.id.action_three) {
+            ghostIV.setColorFilter(Color.argb(100, 0, 0, 255));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
